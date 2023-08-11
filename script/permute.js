e8e292359882c2e3f3212c53360d53b002cc33f7
小港 <6534314+xiaogang8@user.noreply.gitee.com>
2023-08-11 21:49:56 +0800

    1111

--- script/permute.js
+++ script/permute.js
@@ -1,51 +1,78 @@
 var obj = JSON.parse($response.body);
-
-obj = {
-  success: true,
-  response: {
-    checkout_secret_key: "12qqk3q",
-    vendor_name: "Charlie Monroe Software",
-    discount_value: {
-      UAH: 0,
-      USD: 0,
-      GBP: 0,
-      AUD: 0,
-      CNY: 0,
-      EUR: 0,
+if (obj.response.checkout_secret_key) {
+  obj = {
+    success: true,
+    response: {
+      checkout_secret_key: "12qqk3q",
+      vendor_name: "Charlie Monroe Software",
+      discount_value: {
+        UAH: 0,
+        USD: 0,
+        GBP: 0,
+        AUD: 0,
+        CNY: 0,
+        EUR: 0,
+      },
+      trial: {
+        text: "Thanks for downloading a trial of Permute 3! We hope you enjoy it.",
+        duration: 7,
+      },
+      payment_url: "https://pay.paddle.com/checkout/framework/544716",
+      image:
+        "https://paddle.s3.amazonaws.com/user/156/QEXBL6MQQQ6DXq9FGSXL_Icon-MacOS-512x512%402x.png",
+      base_prices: {
+        UAH: 0,
+        USD: 0,
+        GBP: 0,
+        AUD: 0,
+        CNY: 0,
+        EUR: 0,
+      },
+      type: "sdk-product",
+      current_prices: {
+        UAH: 0,
+        USD: 0,
+        GBP: 0,
+        AUD: 0,
+        CNY: 0,
+        EUR: 0,
+      },
+      name: "Permute 3",
+      discount_percentage: {
+        UAH: 0,
+        USD: 0,
+        GBP: 0,
+        AUD: 0,
+        CNY: 0,
+        EUR: 0,
+      },
     },
-    trial: {
-      text: "Thanks for downloading a trial of Permute 3! We hope you enjoy it.",
-      duration: 7,
+  };
+} else if (obj.response.customer_country) {
+  obj = {
+    success: true,
+    response: {
+      customer_country: "JP",
+      products: [
+        {
+          product_id: 544716,
+          price: {
+            gross: 14.99,
+            net: 13.630000000000001,
+            tax: 1.3600000000000001,
+          },
+          vendor_set_prices_included_tax: true,
+          list_price: {
+            gross: 14.99,
+            net: 13.630000000000001,
+            tax: 1.3600000000000001,
+          },
+          currency: "USD",
+          product_title: "Permute 3",
+          applied_coupon: [],
+        },
+      ],
     },
-    payment_url: "https://pay.paddle.com/checkout/framework/544716",
-    image:
-      "https://paddle.s3.amazonaws.com/user/156/QEXBL6MQQQ6DXq9FGSXL_Icon-MacOS-512x512%402x.png",
-    base_prices: {
-      UAH: 200,
-      USD: 14.99,
-      GBP: 13.99,
-      AUD: 21.989999999999998,
-      CNY: 79,
-      EUR: 14.99,
-    },
-    type: "sdk-product",
-    current_prices: {
-      UAH: 200,
-      USD: 14.99,
-      GBP: 13.99,
-      AUD: 21.989999999999998,
-      CNY: 79,
-      EUR: 14.99,
-    },
-    name: "Permute 3",
-    discount_percentage: {
-      UAH: 0,
-      USD: 0,
-      GBP: 0,
-      AUD: 0,
-      CNY: 0,
-      EUR: 0,
-    },
-  },
-};
+  };
+}
 $done({ body: JSON.stringify(obj) });
