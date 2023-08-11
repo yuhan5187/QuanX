2abff3afef642167cdbe93e2f273a43b62931d31
小港 <6534314+xiaogang8@user.noreply.gitee.com>
2023-08-11 15:00:45 +0800

    新增测试脚本

---
+++ script/mp.js
@@ -0,0 +1,294 @@
+/*
+ *Update Date: 20201123
+ *Collected by Cuttlefish
+ *美颜相机解锁内购
+ *Author: 黑黑酱
+ *Url[https://api.meiyan.com/iap/verify.json]
+ */
+
+var obj = JSON.parse($response.body);
+obj = {
+  base_resp: {
+    err_msg: "ok",
+    ret: 0,
+  },
+  day_cycle_ratio: -0.98613783,
+  last_month_income: 137823,
+  last_week_income: 105724,
+  overview_list: [
+    {
+      ads_stat: [
+        {
+          click_count: 38,
+          exposure_count: 262,
+          income: 2738,
+          req_succ_count: 787,
+          slot_id: "1030436212907001",
+        },
+        {
+          click_count: 13,
+          exposure_count: 276,
+          income: 346,
+          req_succ_count: 763,
+          slot_id: "3030046789020061",
+        },
+        {
+          click_count: 16,
+          exposure_count: 945,
+          income: 426,
+          req_succ_count: 2470,
+          slot_id: "4071202390577885",
+        },
+        {
+          click_count: 62,
+          exposure_count: 261,
+          income: 534,
+          req_succ_count: 503,
+          slot_id: "5060180989186180",
+        },
+      ],
+      daily_income: 4044,
+      date: "2023-08-04",
+    },
+    {
+      ads_stat: [
+        {
+          click_count: 1,
+          exposure_count: 22,
+          income: 726,
+          req_succ_count: 70,
+          slot_id: "1030436212907001",
+        },
+        {
+          click_count: 2,
+          exposure_count: 20,
+          income: 59,
+          req_succ_count: 73,
+          slot_id: "3030046789020061",
+        },
+        {
+          click_count: 2,
+          exposure_count: 70,
+          income: 46,
+          req_succ_count: 260,
+          slot_id: "4071202390577885",
+        },
+        {
+          click_count: 9,
+          exposure_count: 33,
+          income: 115,
+          req_succ_count: 56,
+          slot_id: "5060180989186180",
+        },
+      ],
+      daily_income: 946,
+      date: "2023-08-05",
+    },
+    {
+      ads_stat: [
+        {
+          click_count: 37,
+          exposure_count: 319,
+          income: 8038,
+          req_succ_count: 1183,
+          slot_id: "1030436212907001",
+        },
+        {
+          click_count: 33,
+          exposure_count: 546,
+          income: 2237,
+          req_succ_count: 1706,
+          slot_id: "3030046789020061",
+        },
+        {
+          click_count: 57,
+          exposure_count: 1543,
+          income: 1112,
+          req_succ_count: 4535,
+          slot_id: "4071202390577885",
+        },
+        {
+          click_count: 124,
+          exposure_count: 417,
+          income: 1105,
+          req_succ_count: 885,
+          slot_id: "5060180989186180",
+        },
+      ],
+      daily_income: 12492,
+      date: "2023-08-06",
+    },
+    {
+      ads_stat: [
+        {
+          click_count: 100,
+          exposure_count: 868,
+          income: 20786,
+          req_succ_count: 2498,
+          slot_id: "1030436212907001",
+        },
+        {
+          click_count: 39,
+          exposure_count: 807,
+          income: 2371,
+          req_succ_count: 2604,
+          slot_id: "3030046789020061",
+        },
+        {
+          click_count: 69,
+          exposure_count: 2954,
+          income: 1872,
+          req_succ_count: 8136,
+          slot_id: "4071202390577885",
+        },
+        {
+          click_count: 192,
+          exposure_count: 685,
+          income: 1846,
+          req_succ_count: 1498,
+          slot_id: "5060180989186180",
+        },
+      ],
+      daily_income: 26875,
+      date: "2023-08-07",
+    },
+    {
+      ads_stat: [
+        {
+          click_count: 150,
+          exposure_count: 1349,
+          income: 34180,
+          req_succ_count: 3789,
+          slot_id: "1030436212907001",
+        },
+        {
+          click_count: 44,
+          exposure_count: 938,
+          income: 3503,
+          req_succ_count: 3764,
+          slot_id: "3030046789020061",
+        },
+        {
+          click_count: 112,
+          exposure_count: 4370,
+          income: 3134,
+          req_succ_count: 12191,
+          slot_id: "4071202390577885",
+        },
+        {
+          click_count: 280,
+          exposure_count: 995,
+          income: 2633,
+          req_succ_count: 2347,
+          slot_id: "5060180989186180",
+        },
+      ],
+      daily_income: 43450,
+      date: "2023-08-08",
+    },
+    {
+      ads_stat: [
+        {
+          click_count: 60,
+          exposure_count: 708,
+          income: 13579,
+          req_succ_count: 1984,
+          slot_id: "1030436212907001",
+        },
+        {
+          click_count: 24,
+          exposure_count: 569,
+          income: 1311,
+          req_succ_count: 2092,
+          slot_id: "3030046789020061",
+        },
+        {
+          click_count: 47,
+          exposure_count: 2771,
+          income: 1271,
+          req_succ_count: 7165,
+          slot_id: "4071202390577885",
+        },
+        {
+          click_count: 160,
+          exposure_count: 594,
+          income: 1513,
+          req_succ_count: 1294,
+          slot_id: "5060180989186180",
+        },
+      ],
+      daily_income: 17674,
+      date: "2023-08-09",
+    },
+    {
+      ads_stat: [
+        {
+          click_count: 3,
+          exposure_count: 27,
+          income: 122,
+          req_succ_count: 68,
+          slot_id: "1030436212907001",
+        },
+        {
+          click_count: 0,
+          exposure_count: 28,
+          income: 15,
+          req_succ_count: 83,
+          slot_id: "3030046789020061",
+        },
+        {
+          click_count: 2,
+          exposure_count: 100,
+          income: 40,
+          req_succ_count: 291,
+          slot_id: "4071202390577885",
+        },
+        {
+          click_count: 7,
+          exposure_count: 29,
+          income: 68,
+          req_succ_count: 65,
+          slot_id: "5060180989186180",
+        },
+      ],
+      daily_income: 245,
+      date: "2023-08-10",
+    },
+  ],
+  total_income: 123123,
+  week_cycle_ratio: -0.989773344,
+  yesterday_income: 245,
+  yesterday_overview: {
+    ads_stat: [
+      {
+        click_count: 3,
+        exposure_count: 27,
+        income: 122,
+        req_succ_count: 68,
+        slot_id: "1030436212907001",
+      },
+      {
+        click_count: 0,
+        exposure_count: 28,
+        income: 15,
+        req_succ_count: 83,
+        slot_id: "3030046789020061",
+      },
+      {
+        click_count: 2,
+        exposure_count: 100,
+        income: 40,
+        req_succ_count: 291,
+        slot_id: "4071202390577885",
+      },
+      {
+        click_count: 7,
+        exposure_count: 29,
+        income: 68,
+        req_succ_count: 65,
+        slot_id: "5060180989186180",
+      },
+    ],
+  },
+};
+$done({ body: JSON.stringify(obj) });
