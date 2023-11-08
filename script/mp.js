606514eab0a353945f84403d7d5450cd7db28db9
小港 <6534314+xiaogang8@user.noreply.gitee.com>
2023-11-08 11:06:18 +0800

    111

--- script/mp.js
+++ script/mp.js
@@ -6,15 +6,23 @@
  *Url[https://api.meiyan.com/iap/verify.json]
  */
 
-// var ok = {
-//   yesterday_income: 0,
-//   total_income: 144229,
-//   last_week_income: 13,
-//   last_month_income: 13,
-//   total_programmatic_ad_income: 144229,
-//   total_freetrade_income: 0,
-//   total_cps_income: 0,
-// };
+var ok = {
+  bot_status: -1,
+  ds: "20231107",
+  estimate_bot_income: 43.921356935,
+  estimate_mid_income: 32.403627567,
+  mid_status: -1,
+  total_programmatic_ad_income: 213123123,
+  yesterday_income: 3123113,
+  yesterday_income: 3123113,
+  total_income: 144229,
+  last_week_income: 13,
+  last_month_income: 13,
+  total_programmatic_ad_income: 213123123,
+  total_freetrade_income: 0,
+  total_cps_income: 0,
+};
+
 let data = {
   total_income: 213123123,
   total_programmatic_ad_income: 213123123,
